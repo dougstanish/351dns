ae13af220adc6938ef5e64bbbdeafd9c0ca817fc
Spencer Hill <spencernh77@gmail.com>
2019-10-31 23:25:08 -0400

    Make tar be quiet.

--- runme.sh
+++ runme.sh
@@ -1,12 +1,12 @@
 javac src/dns351.java
 echo "ARCHIVE=\`awk '/^__ARCHIVE_BELOW__/ {print NR + 1; exit 0; }' \$0\`" > 351dns
-echo "tail -n+\$ARCHIVE \$0 | tar xzv -C \`pwd\`" >> 351dns
+echo "tail -n+\$ARCHIVE \$0 | tar xz -C \`pwd\`" >> 351dns
 echo "java dns351 \$@" >> 351dns
 echo "rm dns351.class" >> 351dns
 echo "exit \$?" >> 351dns
 echo "__ARCHIVE_BELOW__" >> 351dns
 cd src
-tar -Ocz dns351.class >> ../351dns
+tar -Ocz dns351.class 2>/dev/null >> ../351dns
 rm dns351.class
 cd ..
 chmod +x 351dns
